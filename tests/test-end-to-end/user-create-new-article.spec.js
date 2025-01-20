ad5ace28dd05f0eed503b107e74afd24ae7fa562
Ari Krisnadewi <kadekarikrisnadewi@gmail.com>
2025-01-20 23:46:11 +0800

    update create new article test

--- tests/test-end-to-end/user-create-new-article.spec.js
+++ tests/test-end-to-end/user-create-new-article.spec.js
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 import login from "../helpers/login";
 import logout from "../helpers/logout";
-import { navBar, newArticle } from "../helpers/menu";
+import { homePage, navBar, newArticle } from "../helpers/menu";
 
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
@@ -13,37 +13,33 @@ let testArticle = {
     articleTags: 'test,automation,playwright',
 };
 
-test.describe("Create a New Article on Conduit Realworld Example App", () => {
-    test.beforeEach("Login to the system and navigate to the homepage", async ({ page }) => {
-        // Log in before each test
-        await login(page)(email)(password);
-        await expect(page.locator(navBar.HOME_TAB)).toBeVisible();
-      });
+test("Should create a new article successfully", async ({page}) => {
 
-    test("Should create a new article successfully", async ({page}) => {
-        // Navigate to "New Article"
-        await page.locator(navBar.NEW_ARTICLE_TAB).click();     
+    // Verify login and navigate to the homepage
+    await login(page)(email)(password);
+    await expect(page.locator(homePage.YOUR_FEED_TAB)).toBeVisible();
 
-        // Fill in the "New Article" form
-        await page.locator(newArticle.ARTICLE_TITLE).fill(testArticle.articleTitle);
-        await page.locator(newArticle.ARTICLE_DESCRIPTION).fill(testArticle.articleDescription);
-        await page.locator(newArticle.ARTICLE_BODY).fill(testArticle.articleBody);
-        await page.locator(newArticle.ARTICLE_TAGS).fill(testArticle.articleTags);
-            
-        // Verify Publish the Article
-        await page.locator(newArticle.PUBLISH_ARTICLE_BTN).click();
-            
-        // Verify that the article is published
-        await page.waitForSelector('h1'); // Wait for the article title to appear
-        const publishedTitle = await page.locator('h1').textContent();
-        expect(publishedTitle).toBe(testArticle.articleTitle);
-            
-        console.log('Article published successfully');
-    });
 
-    test.afterEach("Go to home page", async ({page}) => {
-        await logout(page); 
-    });
+    // Navigate to "New Article"
+    await page.locator(navBar.NEW_ARTICLE_TAB).click();     
 
+    // Fill in the "New Article" form
+    await page.locator(newArticle.ARTICLE_TITLE).fill(testArticle.articleTitle);
+    await page.locator(newArticle.ARTICLE_DESCRIPTION).fill(testArticle.articleDescription);
+    await page.locator(newArticle.ARTICLE_BODY).fill(testArticle.articleBody);
+    await page.locator(newArticle.ARTICLE_TAGS).fill(testArticle.articleTags);
+        
+    // Verify Publish the Article
+    await page.locator(newArticle.PUBLISH_ARTICLE_BTN).click();
+        
+    // Verify that the article is published
+    await page.waitForSelector('h1'); // Wait for the article title to appear
+    const publishedTitle = await page.locator('h1').textContent();
+    expect(publishedTitle).toBe(testArticle.articleTitle);
+        
+    console.log('Article published successfully');
+
+    // Verify logout after test
+    await logout(page); 
 });
 
