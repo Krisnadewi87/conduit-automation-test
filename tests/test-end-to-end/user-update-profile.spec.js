cbba9e65c152d4133f4b1e343a00df68c61ae387
Ari Krisnadewi <kadekarikrisnadewi@gmail.com>
2025-01-21 00:58:31 +0800

    small updates on test script

--- tests/test-end-to-end/user-update-profile.spec.js
+++ tests/test-end-to-end/user-update-profile.spec.js
@@ -1,4 +1,4 @@
-import test, { expect } from "playwright/test";
+import { test, expect } from "playwright/test";
 import { navBar, profilePage } from "../helpers/menu";
 import { resetPage } from "../helpers/reset-page";
 import signUp from "../helpers/sign-up";
