057cead451ec766bfe4ab03f554518bc27258480
Ari Krisnadewi <kadekarikrisnadewi@gmail.com>
2025-01-22 00:29:30 +0800

    update comment article test

--- tests/test-functionality/user-comment-article.spec.js
+++ tests/test-functionality/user-comment-article.spec.js
@@ -6,6 +6,9 @@ test("Should add a comment to an article successfully", async ({page}) => {
     const email = process.env.EMAIL;
     const password = process.env.PASSWORD;
     const username = process.env.USER_NAME;
+    const baseURL = process.env.URL
+    const articleSlug = "article-comment"; // Parameterized article slug
+    const commentText = `This is a test comment at ${new Date().toISOString()}`;
 
     // Verify login
     await login(page)(email)(password);
@@ -18,15 +21,13 @@ test("Should add a comment to an article successfully", async ({page}) => {
     // Verify Global Feed tab on the Home page visible
     await page.locator(homePage.GLOBAL_FEED_TAB).click();
 
-    // Go to the specific artcile
-    const baseURL = process.env.URL
-    await page.goto(`${baseURL}article/article-comment`)
+    // Navigate to the article
+    await page.goto(`${baseURL}article/${articleSlug}`)
 
     // Verify the article page visible
     await expect(page.locator('h1').getByText('Article Comment')).toBeVisible();
 
     // Add a comment to the article
-    const commentText = `This is a test comment at ${new Date().toISOString()}`;
     await page.locator(articlePage.COMMENT_FIELD).fill(commentText);
 
     // Verify post the comment
